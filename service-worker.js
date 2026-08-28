e6722b24be30e46e4ae7fce7757e6c7726e3af7f
raafarosa <141688193+raafarosa@users.noreply.github.com>
2026-08-28 14:07:08 -0300

    Alter sw

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+
+// Instalação: Salva os arquivos essenciais
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Força a atualização imediata
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('SW: Cache Instalado');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Ativação: Limpa o lixo (caches antigos)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('SW: Removendo cache antigo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Busca (Fetch): Estratégia Network-First
+// Ele tenta a rede. Se falhar (offline), pega no cache.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
